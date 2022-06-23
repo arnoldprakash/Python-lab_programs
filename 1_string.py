2cee1ec2afbaed9f30da9807c827298c7f150aeb
Arnold Prakash <arnoldprakash@gmail.com>
2022-06-23 14:42:45 +0530

    1modified

--- 1_string.py
+++ 1_string.py
@@ -1,2 +1,20 @@
 #demonstrate string slicing and 5 string functions
+#1. string slicing
+print("string slicing")
+a="good moring"
+print(a[0::])
+print(a[1:-1:])
+print(a[-3::1])
+print(a[4:-1:])
 
+
+#string functions
+str="Hai"
+print("length :",len(str))
+poem="all that does flow"
+print(poem[:10])
+print(len(poem))
+print(poem.startswith("all"))
+print(poem.endswith("folks"))
+print(poem.capitalize())
+print(poem.upper())
